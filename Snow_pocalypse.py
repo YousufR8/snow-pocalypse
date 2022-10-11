bc42705cc063dd291eff00e89ba16149ba0fa1e2
YousufR8 <111895919+YousufR8@users.noreply.github.com>
2022-10-11 12:42:13 -0400

    Add files via upload

---
+++ Snow_pocalypse.py
@@ -0,0 +1,41 @@
+from requests_html import HTMLSession
+s = HTMLSession()
+
+query = 'youngstown'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'})
+
+temp = r.html.find('span#wob_tm', first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+print(query, temp, unit, desc)
+
+from requests_html import HTMLSession
+s = HTMLSession()
+
+query = 'youngstown'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'})
+
+temp = r.html.find('span#wob_tm', first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+print(query, temp, unit, desc)
+
+from requests_html import HTMLSession
+s = HTMLSession()
+
+query = 'youngstown'
+url = f'https://www.google.com/search?q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'})
+
+temp = r.html.find('span#wob_tm', first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+print(query, temp, unit, desc)
